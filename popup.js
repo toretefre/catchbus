e35b51243f6e105d5867d5dbf3620c260ad7646d
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-06-08 09:49:11 +0200

    added large picture and some js

---
+++ popup.js
@@ -0,0 +1,5 @@
+// document.addEventListener('DOMContentLoaded', function()
+document.getElementById("myButton").onclick(
+    "location.href = \'https://www.atb.no/reiseplanlegger/?tab=realtime'")
+;
+
