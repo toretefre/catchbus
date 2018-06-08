b759b3587a3ab639e932d222bacb8801d4240ec2
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-06-08 13:33:27 +0200

    changed from js button to html form button

--- popup.js
+++
@@ -1,5 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function()
-document.getElementById("myButton").onclick(
-    "location.href = \'https://www.atb.no/reiseplanlegger/?tab=realtime'")
-;
-
