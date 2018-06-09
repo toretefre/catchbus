23732a1d250842c6a226cbd98cfa9ad7fce3f28c
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-06-09 17:49:05 +0200

    tried to make a form with autocomplete to request real time station data

--- popup.js
+++ popup.js
@@ -1,6 +1,24 @@
+/*
+Sample JQuery code
 $( document ).ready(function() {
         $( "a" ).click(function( event ) {
             alert( "JQuery er installert og vil snart gje deg sanntidsdata!" );
             event.preventDefault();
+        });
+    });
+*/
+
+    $( function() {
+        var availableStations = [
+            "Gløshaugen Syd",
+            "Dragvoll",
+            "Munkegata",
+            "Samfundet",
+            "Studentersamfundet",
+            "Prinsenkrysset"
+        ];
+
+        $("#stations").autocomplete({
+            source: availableStations
         });
     });
