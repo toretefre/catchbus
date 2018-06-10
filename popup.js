1c58317df85fa3b67d1cc92377a523864135ec6f
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-06-10 21:51:16 +0200

    removed autocomplete

--- popup.js
+++ popup.js
@@ -1,24 +1,23 @@
-/*
-Sample JQuery code
 $( document ).ready(function() {
         $( "a" ).click(function( event ) {
             alert( "JQuery er installert og vil snart gje deg sanntidsdata!" );
             event.preventDefault();
         });
     });
-*/
 
-    $( function() {
-        var availableStations = [
-            "Gløshaugen Syd",
-            "Dragvoll",
-            "Munkegata",
-            "Samfundet",
-            "Studentersamfundet",
-            "Prinsenkrysset"
-        ];
 
-        $("#stations").autocomplete({
-            source: availableStations
-        });
-    });
+var resultText = document.getElementById("resultText");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        resultText.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+
+function showPosition(position) {
+    resultText.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
