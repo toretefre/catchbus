01759f3b1eadb6ffd978b526877d4cc70821eba7
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-06-12 22:24:17 +0200

    added function for calculating distance and some test coordinates to test it

--- popup.js
+++ popup.js
@@ -15,5 +15,34 @@ function showLocation(latitude, longitude) {
     console.log( "JQuery has displayed location!" )
 }
 
+var testUserLat = 63.422557
+var testUserLon = 10.397147
+
+var prinsenkinoLat = 63.426319
+var prinsenkinoLon = 10.393601
+
+var samfundetLat = 63.422609
+var samfundetLon = 10.394647
+
+
+function degreesToRadians(degrees) {
+  return degrees * Math.PI / 180;
+}
+
+function distanceInMetersBetweenEarthCoordinates(userLat, userLon, stopLat, stopLon) {
+  const earthRadiusKm = 6371;
+
+  var dLat = degreesToRadians(stopLat - userLat);
+  var dLon = degreesToRadians(stopLon - userLon);
+
+  userLat = degreesToRadians(userLat);
+  stopLat = degreesToRadians(stopLat);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(userLat) * Math.cos(stopLat);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  return (earthRadiusKm * c) * 1000;
+}
+
 // Not needed because showPosition is triggered on load
 // document.getElementById("geoStationButton").addEventListener("click", showPosition);
