8908c76024bb4a68006e1d8dd39a035322d60b95
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-06-13 20:22:28 +0200

    improved front end and tried to implement time until departure

--- popup.js
+++ popup.js
@@ -1,26 +1,38 @@
 var resultText = document.getElementById("resultText");
+var stations = document.getElementById("stations");
 
 $( document ).ready(function() {
     console.log( "JQuery has started!" );
     navigator.geolocation.getCurrentPosition(function(position) {
-        var user = ["Din posisjon", position.coords.latitude, position.coords.longitude];
-        showLocation(user, nameOfStop(samfundet), distanceInMetersBetweenUserAndStop(user, samfundet));
+        // Constructing test data
+        var departureTime = new Date().getTime();
+        var routeNumber = 5;
+        var destination = "Dragvoll/Lohove";
+        var user = ["Brukerposisjon", position.coords.latitude, position.coords.longitude];
+        var departure = [routeNumber, destination, departureTime];
+        showLocation(user, nameOfStop(samfundet), distanceInMetersBetweenUserAndStop(user, samfundet), departure);
     });
     console.log( "JQuery has polled for location!" );
 });
 
 
-function showLocation(user, closestStop, distanceToClosestStop) {
+function showLocation(user, closestStop, distanceToClosestStop, departure) {
+    // test data
+    var currentTime = new Date();
+    var timeUntilDeparture = departure[2] - currentTime;
+
+    stations.placeholder = closestStop;
     resultText.innerHTML =  "<span>Latitude: " + user[1].toFixed(6) + "</span>" +
                             "<span><br>Longitude: " + user[2].toFixed(6) + "</span>" +
-                            "<span><br>Næraste haldeplass: " + closestStop + "</span>" +
-                            "<span><br>Distanse: " + distanceToClosestStop + " meter" + "</span>";
+                            "<span><br>Neste " + departure[0] + " mot " + departure[1] + " kjem om" +
+                            "<br>" + timeUntilDeparture + " minutt</span>" +
+                            "<span><br>Du er " + distanceToClosestStop + " meter unna " + closestStop + "</span>";
     console.log( "JQuery has displayed location!" )
 }
 
 // [name, lat, lon]
 var prinsenkino = ["Prinsen Kinosenter", 63.426319, 10.393601];
-var samfundet = ["Studentersamfundet", 63.422609, 10.394647];
+var samfundet = ["Samfundet", 63.422609, 10.394647];
 
 
 function nameOfStop(stop) {
@@ -51,7 +63,7 @@ function distanceInMetersBetweenUserAndStop(user, stop) {
     var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
             Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(userLat) * Math.cos(stopLat);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    return ((earthRadiusKm * c) * 1000).toFixed(1);
+    return ((earthRadiusKm * c) * 1000).toFixed(0);
 }
 
 // Not currently needed because showPosition is triggered on load
