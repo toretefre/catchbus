eaca4b536b7200c6e54e261da57bd79d8b5171ca
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2018-06-17 13:34:10 +0200

    JSON station data now being fetched correctly + a lot of commenting

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
 var resultText = document.getElementById("resultText");
-var jsondata;
+var closestStops = [];
 
+// Executed when document is loaded
 $( document ).ready(function() {
-    console.log( "Document loaded, starting calls!" );
+    console.log( "Document loaded, polling for position..." );
     navigator.geolocation.getCurrentPosition(function(position) {
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
-        getNearestStop(latitude, longitude);
-        console.log( "getNearestStop has been called!" );
+        console.log("Position found, calling getNearestStops with coords " + latitude + ", " + longitude);
+        getNearestStops(latitude, longitude);
     });
 });
 
-
+// Changes the HTML when data is ready
 function showLocation() {
     console.log("showLocation called!");
     // Constructing test data
@@ -28,29 +29,55 @@ function showLocation() {
                             timeUntilDeparture(departure[3], now) + "</span>";
 
     console.log("showLocation finished!");
-    console.log("JSON-data: " + jsondata);
 }
 
+// Defines how many nearby stops to locate:
+var numberOfStops = 2;
+// Defines what kind of stations to locate, onstreetBus is only bus
+var mode = "onstreetBus";
 
-function getNearestStop(latitude, longitude) {
+
+// Connects with Entur-API to find nearby stations based on coordinates
+function getNearestStops(latitude, longitude) {
     var xhr = new XMLHttpRequest();
-    console.log(latitude + " " + longitude);
+    console.log("getNearestStops called with coords " + latitude + ", " + longitude);
     xhr.open("GET", "https://api.entur.org/api/geocoder/1.1/reverse?point.lat=" +
             latitude + "&point.lon=" + longitude +
-            "&lang=en&size=10&layers=venue&category=onstreetBus");
+            "&lang=en&size=" + numberOfStops + "&layers=venue&category=" + mode);
     xhr.setRequestHeader("Content-Type", "text/json");
     xhr.send();
-    console.log("XMLHttpRequest sent!");
+    console.log("XMLHttpRequest to find the " + numberOfStops + " closest stops sent!");
     xhr.onreadystatechange = function () {
         if(xhr.readyState === 4 && xhr.status === 200) {
-            console.log("Operation done, 200 received!");
-            jsondata = xhr.response;
+            console.log("XMLHttpRequest returned the following JSON with status code 200:");
+            parseJSONData(xhr.response);
             showLocation();
         }
     }
 }
 
 
+// Parses JSON station data received from Entur into Javascript objects
+function parseJSONData(jsonToParse) {
+    console.log("Raw JSON: " + jsonToParse);
+    var parsedJSON = JSON.parse(jsonToParse);
+    console.log("JSON parsed! List of IDs:");
+    for (var id in parsedJSON) {
+        if (parsedJSON.hasOwnProperty(id)) {
+            var stopName = "";
+            var stopID = -1;
+            var distance = -1;
+            var latitude = -1;
+            var longitude = -1;
+            console.log(id);
+            closestStops.push([stopName, stopID, distance, latitude, longitude]);
+        }
+    }
+    console.log("Array containg closest stops: " + closestStops);
+}
+
+
+// Translates minutes until departure to readable text
 function timeUntilDeparture(departureTime, now) {
     var minutesUntil = (departureTime.getTime() - now.getTime()) / 60000;
 
@@ -72,7 +99,7 @@ function timeUntilDeparture(departureTime, now) {
     }
 }
 
-
+// Translates digit representation into form of transportation
 function findMode(mode) {
     switch (mode) {
         case 0:
